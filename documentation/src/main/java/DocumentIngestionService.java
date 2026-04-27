263283165ca3f8cb7db96fdbe524490ef641d4f9
Pontus Häggblom <pbhaggblom@gmail.com>
2026-04-27 17:24:20 +0200

    Update ingestion method for grpc use

--- documentation/src/main/java/DocumentIngestionService.java
+++ documentation/src/main/java/DocumentIngestionService.java
@@ -1,4 +1,3 @@
-import static dev.langchain4j.data.document.splitter.DocumentSplitters.recursive;
 import static dev.langchain4j.store.embedding.filter.MetadataFilterBuilder.metadataKey;
 
 import java.nio.charset.StandardCharsets;
@@ -14,26 +13,36 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import dev.langchain4j.data.embedding.Embedding;
 import dev.langchain4j.store.embedding.EmbeddingSearchRequest;
 import dev.langchain4j.store.embedding.filter.Filter;
+import io.grpc.Status;
+import io.quarkus.grpc.GrpcService;
+import io.smallrye.common.annotation.Blocking;
+import io.smallrye.mutiny.Multi;
 import jakarta.inject.Inject;
+import jakarta.inject.Singleton;
 import org.eclipse.microprofile.config.inject.ConfigProperty;
+import org.pbhaggblom.DocumentationService;
 
 import dev.langchain4j.data.document.Document;
 import dev.langchain4j.data.document.loader.FileSystemDocumentLoader;
 import dev.langchain4j.model.embedding.EmbeddingModel;
 import dev.langchain4j.store.embedding.chroma.ChromaEmbeddingStore;
 import dev.langchain4j.store.embedding.EmbeddingStoreIngestor;
-import io.quarkus.logging.Log;
-import jakarta.enterprise.context.ApplicationScoped;
+import org.pbhaggblom.IngestionRequest;
+import org.pbhaggblom.IngestionResponse;
 
 
-@ApplicationScoped
-public class DocumentIngestionService {
+@GrpcService
+@Singleton
+public class DocumentIngestionService implements DocumentationService {
 
     @Inject
     ChromaEmbeddingStore store;
 
     @Inject
     EmbeddingModel embeddingModel;
+
+    @Inject
+    EmbeddingStoreIngestor ingestor;
 
     @ConfigProperty(name = "rag.location")
     Path path;
@@ -42,41 +51,6 @@ public class DocumentIngestionService {
     private static final Embedding DUMMY_EMBEDDING = Embedding.from(ZERO_VECTOR);
 
     private final AtomicBoolean isRunning = new AtomicBoolean(false);
-
-    public void ingest() {
-        if (!isRunning.compareAndSet(false, true)) {
-            throw new IllegalStateException("Ingestion is already running");
-        }
-
-        PathMatcher matcher = p -> p.getFileName().toString().endsWith(".md");
-        List<Document> list = loadDocuments(path, matcher);
-
-        for (Document doc : list) {
-            String fileName = doc.metadata().getString("file_name");
-            String currentHash = calculateHash(doc.text());
-
-            if (isAlreadyIndexed(fileName, currentHash)) {
-                Log.info("Skipping " + fileName + " - no changes detected.");
-                continue;
-            }
-
-            Log.info("Changes detected in " + fileName + ". Updating index...");
-
-            store.removeAll(metadataKey("file_name").isEqualTo(fileName));
-
-            doc.metadata().put("file_hash", currentHash);
-            doc.metadata().put("last_updated", LocalDateTime.now().toString());
-
-            EmbeddingStoreIngestor ingestor = EmbeddingStoreIngestor.builder()
-                    .embeddingStore(store)
-                    .embeddingModel(embeddingModel)
-                    .documentSplitter(recursive(500, 75))
-                    .build();
-            ingestor.ingest(doc);
-
-        }
-        Log.info("Documents ingested successfully");
-    }
 
     private List<Document> loadDocuments(Path path, PathMatcher matcher) {
         List<Document> list = FileSystemDocumentLoader.loadDocumentsRecursively(path, matcher);
@@ -106,4 +80,38 @@ public class DocumentIngestionService {
         }
     }
 
+    @Blocking
+    @Override
+    public Multi<IngestionResponse> startIngestion(IngestionRequest request) {
+        if (!isRunning.compareAndSet(false, true)) {
+            throw Status.ALREADY_EXISTS
+                    .withDescription("Ingestion is already running")
+                    .asRuntimeException();
+        }
+
+        PathMatcher matcher = p -> p.getFileName().toString().endsWith(".md");
+
+        return Multi.createFrom().iterable(loadDocuments(path, matcher))
+                .onItem().transform(doc -> {
+
+                    String fileName = doc.metadata().getString("file_name");
+                    String currentHash = calculateHash(doc.text());
+
+                    if (isAlreadyIndexed(fileName, currentHash)) {
+                        String res = "Skipping " + fileName + " - no changes detected.";
+                        return IngestionResponse.newBuilder().setResponse(res).build();
+                    }
+
+                    String res = "Changes detected in " + fileName + ". Updating index...";
+
+                    store.removeAll(metadataKey("file_name").isEqualTo(fileName));
+
+                    doc.metadata().put("file_hash", currentHash);
+                    doc.metadata().put("last_updated", LocalDateTime.now().toString());
+                    ingestor.ingest(doc);
+
+                    return IngestionResponse.newBuilder().setResponse(res).build();
+                })
+                .onTermination().invoke(() -> isRunning.set(false));
+    }
 }
