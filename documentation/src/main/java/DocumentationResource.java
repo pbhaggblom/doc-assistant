5f5dd3feefd28ad394a7817512681aa5cc0e1c7d
Pontus Häggblom <pbhaggblom@gmail.com>
2026-04-27 08:23:36 +0200

    Add cli module

---
+++ documentation/src/main/java/DocumentationResource.java
@@ -0,0 +1,22 @@
+import dev.langchain4j.store.embedding.chroma.ChromaEmbeddingStore;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.Path;
+
+@Path("/doc")
+public class DocumentationResource {
+
+    @Inject
+    ChromaEmbeddingStore store;
+
+    @Inject
+    DocumentIngestionService ingestor;
+
+    @Path("/ingest")
+    public void ingest() {
+        ingestor.ingest();
+    }
+
+//    public void getStoreInfo() {
+//        store.
+//    }
+}
