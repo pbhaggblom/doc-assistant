bd82908adda728134cab298a20435d87db32c9e8
Pontus Häggblom <pbhaggblom@gmail.com>
2026-05-10 19:58:41 +0200

    add ResetCommand to entrypoint

--- cli/src/main/java/EntryPoint.java
+++ cli/src/main/java/EntryPoint.java
@@ -3,7 +3,7 @@ import picocli.CommandLine.Command;
 
 @TopCommand
 @Command(name = "doc",
-        subcommands = {AssistantCommand.class, IngestionCommand.class, StatusCommand.class},
+        subcommands = {AssistantCommand.class, IngestionCommand.class, StatusCommand.class, ResetCommand.class},
         mixinStandardHelpOptions = true,
         description = "Assistant that answers questions about Kubernetes")
 public class EntryPoint {
