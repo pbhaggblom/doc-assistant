debf12a710d86a504d62cf418a7b0230012b50ba
Pontus Häggblom <pbhaggblom@gmail.com>
2026-05-10 19:52:20 +0200

    add ResetCommand

---
+++ cli/src/main/java/ResetCommand.java
@@ -0,0 +1,28 @@
+import io.quarkus.grpc.GrpcClient;
+import org.pbhaggblom.documentation.DocumentationServiceGrpc.DocumentationServiceBlockingStub;
+import org.pbhaggblom.documentation.ResetRequest;
+import org.pbhaggblom.documentation.ResetResponse;
+import picocli.CommandLine.Command;
+
+import java.util.concurrent.Callable;
+
+@Command(name = "status", description = "Resets database. Requires admin access.")
+public class ResetCommand implements Callable<Integer> {
+
+    @GrpcClient("ingestor")
+    DocumentationServiceBlockingStub documentService;
+
+    @Override
+    public Integer call() throws Exception {
+        try {
+            ResetRequest request = ResetRequest.newBuilder().build();
+            ResetResponse response = documentService.clearDatabase(request);
+            System.out.println(response.getResponse());
+            System.out.flush();
+            return 0;
+        } catch (Exception e) {
+            System.err.println("Error while resetting database: " + e.getMessage());
+            return 1;
+        }
+    }
+}
